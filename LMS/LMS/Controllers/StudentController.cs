4e81e5668b887b2f793bf1efb1ddad3469c94e7b
rahta attique <rahatattique@gmail.com>
2019-04-30 11:19:24 +0500

    employee details,student details

---
+++ LMS/LMS/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMS.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult StudentDetails()
+        {
+            if (Session["LoginID"] != null)
+            {
+                return View();
+
+            }
+            else
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+
+        }
+    }
+}
